daae01b44147ae70114a487cbabcb34443b1e50a
AdiUser <adityasaxena602@gmail.com>
2019-03-09 17:47:45 +0530

    ratings object `ratings` added

--- application/views/profile.php
+++ application/views/profile.php
@@ -5,148 +5,12 @@
         <title>Document</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-typeahead/2.10.6/jquery.typeahead.min.css"/>
+        <link rel="stylesheet" href="assets/css/main.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
         <script src="assets/js/typeahead.js"></script>
         <style>
-            html.ios {
-                overflow-x: hidden;
-                -webkit-overflow-scrolling: touch;
-            }
-
-            html.ios, html.ios body {
-                height: initial !important;
-            }
-
-            .width-max {
-                width: 70%;
-            }
-
-            .twitter-typeahead {
-                width: 100%;
-            }
-
-            .fancy-input {
-                position: relative;
-                vertical-align: top;
-                margin: 0;
-                width: 100%;
-                max-width: 100%;
-                -webkit-box-flex: 1;
-                -ms-flex: 1 0 auto;
-                flex: 1 0 auto;
-                outline: 0;
-                -webkit-tap-highlight-color: rgba(255,255,255,0);
-                text-align: left;
-                line-height: 1.21428571em;
-                font-family: Lato,'Helvetica Neue',Arial,Helvetica,sans-serif;
-                padding: .67857143em 1em;
-                background: #fff;
-                border: 1px solid rgba(34,36,38,.15);
-                color: rgba(0,0,0,.87);
-                border-radius: .28571429rem;
-                -webkit-transition: border-color .1s ease,-webkit-box-shadow .1s ease;
-                transition: border-color .1s ease,-webkit-box-shadow .1s ease;
-                transition: box-shadow .1s ease,border-color .1s ease;
-                transition: box-shadow .1s ease,border-color .1s ease,-webkit-box-shadow .1s ease;
-                -webkit-box-shadow: none;
-                box-shadow: none;
-                background-color: transparent;
-            }
-
-            .tt-query, /* UPDATE: newer versions use tt-input instead of tt-query */
-            .tt-hint {
-                width: 396px;
-                height: 30px;
-                padding: 8px 12px;
-                font-size: 24px;
-                line-height: 30px;
-                border: 2px solid #ccc;
-                border-radius: 8px;
-                outline: none;
-            }
-
-            .tt-query {
-                /* UPDATE: newer versions use tt-input instead of tt-query */
-                box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-            }
-
-            .tt-hint {
-                color: #999;
-                padding: .67857143em 1em;
-                height: 100%;
-            }
-
-            .tt-menu {
-                /* UPDATE: newer versions use tt-menu instead of tt-dropdown-menu */
-                width: 422px;
-                margin-top: 12px;
-                padding: 8px 0;
-                background-color: #fff;
-                border: 1px solid #ccc;
-                border: 1px solid rgba(0, 0, 0, 0.2);
-                border-radius: 8px;
-                box-shadow: 0 5px 10px rgba(0,0,0,.2);
-            }
-
-            .tt-suggestion {
-                padding: 3px 20px;
-                font-size: 18px;
-                line-height: 24px;
-            }
-
-            .tt-suggestion.tt-is-under-cursor {
-                /* UPDATE: newer versions use .tt-suggestion.tt-cursor */
-                color: #fff;
-                background-color: #0097cf;
-            }
-
-            .tt-suggestion p {
-                margin: 0;
-            }
-
-            span.sp {
-                font-size: 1.2em;
-                color: #484242;
-            }
-
-            .header-cstm {
-                font-size: 1.3em;
-                margin: 0 0 5px 0;
-            }
-
-            .centered {
-                position: absolute !important;
-                top: 50%;
-                left: 50%;
-                width: 50%;
-                transform: translate(-50%, -100%);
-            }
-
-            .adjust-top {
-                margin-top: 0px !important;
-            }
-
-            #change-content {
-                margin-top: 40px;
-                margin-bottom: 40px;
-                text-align: center;
-                min-height: 256px;
-            }
-
-            .btn {
-                text-align: center;
-            }
-
-            .table {
-                width: fit-content !important;
-                margin: auto !important;
-            }
-
-            .steps-contain {
-                width: fit-content;
-                margin: auto;
-            }
+         
         </style>
     </head>
     <body>
@@ -227,7 +91,7 @@
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/lena.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Puntuality
             <div class="sub header">Faculty arrives and leaves the class on time.
@@ -236,13 +100,13 @@
                                         </h4>
                                     </td>
                                     <td>
-                                        <div class="ui massive star rating"></div>
+                                        <div class="ui massive star rating" id="puntuality"></div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/matthew.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Knowledgeable
             <div class="sub header">Matery over the subject this faculty teaches.
@@ -251,13 +115,13 @@
                                         </h4>
                                     </td>
                                     <td>
-                                        <div class="ui massive star rating"></div>
+                                        <div class="ui massive star rating" id="knowledge"></div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/lindsay.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Textbook Use
             <div class="sub header">Faculty provides study notes and material.
@@ -266,13 +130,13 @@
                                         </h4>
                                     </td>
                                     <td>
-                                        <div class="ui massive star rating"></div>
+                                        <div class="ui massive star rating" id="textbook_use"></div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/lindsay.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Teaching Speed
             <div class="sub header">Ability to finish course on time.
@@ -281,7 +145,7 @@
                                         </h4>
                                     </td>
                                     <td>
-                                        <div class="ui massive star rating"></div>
+                                        <div class="ui massive star rating" id="teaching_speed"></div>
                                     </td>
                                 </tr>
                             </tbody>
@@ -299,7 +163,7 @@
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/lena.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Doubt Clearing
             <div class="sub header">Time given for doubt clearing sessions.
@@ -308,13 +172,13 @@
                                         </h4>
                                     </td>
                                     <td>
-                                        <div class="ui massive star rating"></div>
+                                        <div class="ui massive star rating" id="doubt_clearing"></div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/matthew.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Helping In Class
             <div class="sub header">Assisting students when needed
@@ -323,13 +187,13 @@
                                         </h4>
                                     </td>
                                     <td>
-                                        <div class="ui massive star rating"></div>
+                                        <div class="ui massive star rating" id="helping"></div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/lindsay.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Proper Clothing
             <div class="sub header">Dresses in an appropriate attire
@@ -338,7 +202,7 @@
                                         </h4>
                                     </td>
                                     <td>
-                                        <div class="ui massive star rating"></div>
+                                        <div class="ui massive star rating" id="clothing"></div>
                                     </td>
                                 </tr>
                             </tbody>
@@ -356,7 +220,7 @@
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/lena.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Describe your Faculty
             <div class="sub header">Select keywords that describe faculty.
@@ -365,7 +229,7 @@
                                         </h4>
                                     </td>
                                     <td style="min-width: 300px;">
-                                        <div class="ui fluid multiple search normal selection dropdown">
+                                        <div class="ui fluid multiple search normal selection dropdown" id="keywords">
                                             <input type="hidden" name="country">
                                             <i class="dropdown icon"></i>
                                             <div class="default text">Select Country</div>
@@ -456,7 +320,7 @@
                                 <tr>
                                     <td>
                                         <h4 class="ui image header">
-                                            <img src="/images/avatar2/small/matthew.png" class="ui mini rounded image">
+                                            
                                             <div class="content">
                                                 Personal Comments
             <div class="sub header">What do you feel about this faculty?
@@ -465,7 +329,7 @@
                                         </h4>
                                     </td>
                                     <td>
-                                        <textarea></textarea>
+                                        <textarea id="personal_remark"></textarea>
                                     </td>
                                 </tr>
                             </tbody>
@@ -496,143 +360,6 @@
             </div>
         </div>
         <?php echo "<pre>" . print_r($faculty, true) ?>
-        <script>
-            $('.ui.rating').rating({
-                initialRating: 3,
-                maxRating: 5
-            });
-            $('.sidebar').sidebar({
-                dimPage: false,
-                transition: 'push',
-                exclusive: false,
-                closable: true
-            });
-
-            $('#show-menu').on('click', function() {
-
-                $('.sidebar').sidebar('toggle');
-            })
-
-            var state = [{
-                "name": "John Snow",
-                "id": 1
-            }, {
-                "name": "Joe Biden",
-                "id": 2
-            }, {
-                "name": "Bob Marley",
-                "id": 3
-            }, {
-                "name": "Anne Hathaway",
-                "id": 4
-            }, {
-                "name": "Jacob deGrom",
-                "id": 5
-            }];
-            var states = new Bloodhound({
-                datumTokenizer: function(data) {
-                	console.log(data)
-                    return Bloodhound.tokenizers.whitespace(data.name)
-                },
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                // `states` is an array of state names defined in "The Basics"
-                local: state
-                /*remote: {
-			        url: 'http://localhost/rating-system/user/get_faculty_data',
-			        transform: function(data) {
-
-			        	return data;
-			        }
-			      }*/
-
-            });
-            ///states.initialize();
-
-            var curScreen = 1;
-            $('.typeahead').typeahead({
-                hint: true,
-                highlight: true,
-                minLength: 1
-            }, {
-                name: 'states',
-                display: 'name',
-                source: states.ttAdapter()
-            });
-            $('.typeahead').bind('typeahead:select', function(ev, suggestion) {
-                console.log(suggestion);
-                $('.rating-modal').modal('show')
-                // open modal and set facultly rating object.
-            });
-
-            $('.rating-modal').modal({
-                onHidden: function() {
-                    curScreen = 1;
-                    $('#screen-btn').show();
-                    $('#screen-btn-submit').hide()
-                    for (let x = 1; x <= 3; x++) {
-                        $('#step-' + x).removeClass('completed');
-                        $('#step-' + x).removeClass('active');
-
-                        if (x == curScreen)
-                            $('.screen-' + x).show();
-                        else
-                            $('.screen-' + x).hide();
-                    }
-
-                }
-            })
-            $('.ui.normal.dropdown').dropdown({
-                maxSelections: 7
-            });
-
-            $('.screen-1').hide();
-            $('.screen-2').hide();
-            $('.screen-3').hide();
-            function showScreen(screen) {
-                console.log(screen)
-                if (screen == 3) {
-                    $('#screen-btn').hide()
-                    $('#screen-btn-submit').show()
-
-                } else {
-                    $('#screen-btn').show()
-                    $('#screen-btn-submit').hide()
-
-                }
-                if (screen > 3)
-                    return false;
-                for (let x = 1; x <= 3; x++) {
-                    if (x == screen) {
-                        $('.screen-' + x).show();
-                        $('#step-' + x).addClass('active')
-                    } else {
-                        $('.screen-' + x).hide();
-                        $('#step-' + x).removeClass('active')
-                    }
-                }
-            }
-
-            showScreen(curScreen);
-            $('#screen-btn-submit').hide()
-            $('#screen-btn').on('click', function() {
-                if (curScreen <= 4) {
-                    showScreen(++curScreen);
-                    $('#step-' + (curScreen - 1)).addClass('completed');
-
-                }
-
-            });
-            $('#screen-btn-back').on('click', function() {
-                //console.log(curScreen)
-                if (curScreen - 1 > 0) {
-                    if (curScreen == 4) {
-                        showScreen(curScreen - 2);
-                        curScreen = curScreen - 2;
-                    } else
-                        showScreen(--curScreen)
-                }
-                return false;
-            })
-        </script>
+        <script src="assets/js/main.js"></script>
     </body>
 </html>
---
+++ assets/js/main.js
@@ -0,0 +1,151 @@
+$(document).ready(function() {
+    var ratings = {};
+    $('.ui.rating').rating('setting', 'onRate', function(value) {
+        var  ratedAttr = $(this)[0];
+        ratings[ratedAttr.id] = value;
+    });
+
+     $('.ui.normal.dropdown').dropdown('setting', 'onChange', function(vale) {
+        var dropdown = $(this)[0];
+        ratings[dropdown.id] = $(this).dropdown('get value');
+     });
+
+     $('#personal_remark').on('change', function() {
+         var textarea = $(this)[0];
+        ratings[textarea.id] = textarea.value;
+     });
+
+     $('#screen-btn-submit').on('click', function() {
+        console.log(ratings);
+     })
+
+    $('.sidebar').sidebar({
+        dimPage: false,
+        transition: 'push',
+        exclusive: false,
+        closable: true
+    });
+
+    $('#show-menu').on('click', function() {
+        $('.sidebar').sidebar('toggle');
+    });
+
+    var state = [{
+        "name": "John Snow",
+        "id": 1
+    }, {
+        "name": "Joe Biden",
+        "id": 2
+    }, {
+        "name": "Bob Marley",
+        "id": 3
+    }, {
+        "name": "Anne Hathaway",
+        "id": 4
+    }, {
+        "name": "Jacob deGrom",
+        "id": 5
+    }];
+    var states = new Bloodhound({
+        datumTokenizer: function(data) {
+            return Bloodhound.tokenizers.whitespace(data.name);
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        // `states` is an array of state names defined in "The Basics"
+        local: state /*remote: {
+                    url: 'http://localhost/rating-system/user/get_faculty_data',
+                    transform: function(data) {
+
+                        return data;
+                    }
+                  }*/
+
+    });
+    ///states.initialize();
+
+    var curScreen = 1;
+    $('.typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    }, {
+        name: 'states',
+        display: 'name',
+        source: states.ttAdapter()
+    });
+    $('.typeahead').bind('typeahead:select', function(ev, suggestion) {
+        $('.rating-modal').modal('show');
+        // open modal and set facultly rating object.
+    });
+
+    $('.rating-modal').modal({
+        onHidden: function() {
+            curScreen = 1;
+
+            $('#screen-btn').show();
+            $('#screen-btn-submit').hide();
+
+            $('.ui.rating').rating('set rating', function(rating) {
+                return 0;
+            });
+            ratings = {};
+            for (var x = 1; x <= 3; x++) {
+                $('#step-' + x).removeClass('compvared');
+                $('#step-' + x).removeClass('active');
+
+                if (x == curScreen)
+                    $('.screen-' + x).show();
+                else
+                    $('.screen-' + x).hide();
+            }
+
+        }
+    });
+   
+
+   // $('.screen-1, .screen-2, .screen-3').hide();
+    function showScreen(screen) {
+       // console.log(screen)
+        if (screen == 3) {
+            $('#screen-btn').hide();
+            $('#screen-btn-submit').show();
+        } else {
+            $('#screen-btn').show();
+            $('#screen-btn-submit').hide();
+
+        }
+        if (screen > 3)
+            return false;
+        for (var x = 1; x <= 3; x++) {
+            if (x == screen) {
+                $('.screen-' + x).show();
+                $('#step-' + x).addClass('active');
+            } else {
+                $('.screen-' + x).hide();
+                $('#step-' + x).removeClass('active');
+            }
+        }
+    }
+
+    showScreen(curScreen);
+    $('#screen-btn-submit').hide();
+    $('#screen-btn').on('click', function() {
+        if (curScreen <= 4) {
+            showScreen(++curScreen);
+            $('#step-' + (curScreen - 1)).addClass('compvared');
+
+        }
+
+    });
+    $('#screen-btn-back').on('click', function() {
+        //console.log(curScreen)
+        if (curScreen - 1 > 0) {
+            if (curScreen == 4) {
+                showScreen(curScreen - 2);
+                curScreen = curScreen - 2;
+            } else
+                showScreen(--curScreen);
+        }
+        return false;
+    });
+});
