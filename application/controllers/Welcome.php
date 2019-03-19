f7c6ab452e67b42b4c3e6e93ba3b9ad16900f8af
AdiUser <adityasaxena602@gmail.com>
2019-03-19 17:56:00 +0530

    new controller

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -51,10 +51,10 @@ class Welcome extends CI_Controller {
 		$this->load->view("faculty_activity");
 	}
 
-	public function import(){
+	public function load_file($file) {
 		$file_mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-		if(isset($_FILES['upload_file']['name']) && in_array($_FILES['upload_file']['type'], $file_mimes)) {
-			$arr_file = explode('.', $_FILES['upload_file']['name']);
+		if(isset($file['name']) && in_array($file['type'], $file_mimes)) {
+			$arr_file = explode('.', $file['name']);
 			$extension = end($arr_file);
 			if('csv' == $extension){
 				$reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
@@ -62,13 +62,16 @@ class Welcome extends CI_Controller {
 			else {
 				$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
 			}
-			$spreadsheet = $reader->load($_FILES['upload_file']['tmp_name']);
+			$spreadsheet = $reader->load($file['tmp_name']);
 			$sheetData = $spreadsheet->getActiveSheet()->toArray();
 			
-			echo "<pre>";
-			print_r($sheetData);
+			return $sheetData;
 	
 		}
+	}
+
+	public function import(){
+		echo "<pre>".print_r($this->load_file($_FILES['upload_file']), true);
 	}
 
 }
